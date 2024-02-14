05382881625bb05f55f0265c0be9f68f58fc0031
JihadSalameh <jihadsalameh.js@gmail.com>
2024-02-14 12:32:20 +0200

    added a profile pic to the Friends List page, and edit the rest of the UI

--- app/src/main/java/com/example/navtestapp/features/home/profile/Profile.kt
+++ app/src/main/java/com/example/navtestapp/features/home/profile/Profile.kt
@@ -1,13 +1,24 @@
 package com.example.navtestapp.features.home.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -16,12 +27,27 @@ fun Profile(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Profile Page")
-        Button(onClick = { navController.popBackStack() }) {
+        Text(
+            modifier = Modifier
+                .wrapContentHeight()
+                .wrapContentWidth(),
+            text = "Profile",
+            fontSize = 26.sp,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Button(
+            onClick = { navController.popBackStack() },
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            modifier = Modifier
+                .clip(shape = RectangleShape)
+                .padding(16.dp, 0.dp, 16.dp, 0.dp)
+        ) {
             Text(text = "Back")
         }
     }
