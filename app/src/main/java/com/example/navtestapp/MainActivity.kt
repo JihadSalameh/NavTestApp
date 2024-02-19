2244c74952808ac341ae5033ed0d119a690f7a23
JihadSalameh <jihadsalameh.js@gmail.com>
2024-02-19 11:21:43 +0200

    edited the color and theme files

--- app/src/main/java/com/example/navtestapp/MainActivity.kt
+++ app/src/main/java/com/example/navtestapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.navtestapp.ui.theme.NavTestAppTheme
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MyViewModel by viewModels()
@@ -19,7 +20,9 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-            AppNav()
+            NavTestAppTheme {
+                AppNav()
+            }
         }
     }
 }
