9fedaf99402ac7d4d52c087f55fc40b141d4c5f9
JihadSalameh <jihadsalameh.js@gmail.com>
2024-02-25 12:39:54 +0200

    added dagger hilt dependencies and setting up

--- app/src/main/java/com/example/navtestapp/MyViewModel.kt
+++ app/src/main/java/com/example/navtestapp/MyViewModel.kt
@@ -2,11 +2,13 @@ package com.example.navtestapp
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
+@HiltViewModel
 class MyViewModel: ViewModel() {
 
     private val _loading = MutableStateFlow(true)
